fa28ada82d43fd0a0a7f136a98158b3e8110d65f
nephridium <nephridium.labs@gmail.com>
2015-07-08 19:05:16 +0800

    Add branch check to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
+
+# check whether in correct branch
+if [ "$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')" != "gh-pages" ]; then
+  echo "Script only runs from 'gh-pages' branch, try again after checkout out branch"
+  echo "git checkout gh-pages"
+  exit 1
+fi
+
 echo Vulcanize
 vulcanize index.html > index.build.html
 mv index.build.html index.html
